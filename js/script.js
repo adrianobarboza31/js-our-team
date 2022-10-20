9d87d5b6752f6fa036ec227a4a731099034f85eb
adrianobarboza31 <barbozaa022@gmail.com>
2022-10-20 17:12:47 +0200

    add bonus senza controlli in input

--- js/script.js
+++ js/script.js
@@ -45,4 +45,42 @@ const dati=[
       },
 
 ]
- 
+const appendi=document.getElementById("appendi")
+ console.log(dati)
+ const card=document.getElementById("card")
+ card.innerHTML=`<img src="./img/${dati[0].image}" alt=""> <br>${dati[0].name}<br>${dati[0].role}`
+//  2
+const card2=document.getElementById("card2")
+card2.innerHTML=`<img src="./img/${dati[1].image}" alt=""> <br>${dati[1].name}<br>${dati[1].role}`
+// 3
+const card3=document.getElementById("card3")
+card3.innerHTML=`<img src="./img/${dati[2].image}" alt=""> <br>${dati[2].name}<br>${dati[2].role}`
+// 4
+const card4=document.getElementById("card4")
+card4.innerHTML=`<img src="./img/${dati[3].image}" alt=""> <br>${dati[3].name}<br>${dati[3].role}`
+// 5
+const card5=document.getElementById("card5")
+card5.innerHTML=`<img src="./img/${dati[4].image}" alt=""> <br>${dati[4].name}<br>${dati[4].role}`
+// 6
+const card6=document.getElementById("card6")
+card6.innerHTML=`<img src="./img/${dati[5].image}" alt=""> <br>${dati[5].name}<br>${dati[5].role}`
+
+const btn=document.getElementById("inserisci")
+btn.addEventListener("click",function invia(){
+    let nome=document.getElementById("nome").value
+let professione=document.getElementById("professione").value
+let immagine=document.getElementById("immagine").value
+    console.log(nome)
+    console.log(professione)
+    console.log(immagine)
+    dati.push({
+        name:nome,
+        role:professione,
+        image:immagine
+    })
+    console.log(dati)
+    const card7= document.createElement("div")
+    card7.classList.add("card")
+    card7.innerHTML=`<img src="./img/${dati[dati.length-1].image}" alt=""> <br>${dati[dati.length-1].name}<br>${dati[dati.length-1].role}`
+    appendi.append(card7)
+})
